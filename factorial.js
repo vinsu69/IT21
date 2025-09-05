93fe897402003806359b3205860289f87d4e8a75
vinsu69 <joewinmante@gmail.com>
2025-09-05 20:13:04 +0800

    initial2

--- factorial.js
+++ factorial.js
@@ -10,3 +10,14 @@ console.log("1. Say Hello");
 console.log("2. Factorial");
 console.log("3. Exit Application");
 console.log("=================================");
+
+rl.question("Enter your choice (1-3): ", choice => {
+    console.log(choice);
+
+    if (choice === '1') {
+        console.log("Hello, User!");
+    } else if (choice === '2') {
+        console.log("Factorial");
+    
+        rl.question("Please enter a number for factorial : ", numStr => {
+            let num = parseInt(numStr);
