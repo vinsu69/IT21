97833a159b65f90cc6b6304c33a8ef686b59adbe
vinsu69 <joewinmante@gmail.com>
2025-09-05 20:14:16 +0800

    factorial.js

--- factorial.js
+++ factorial.js
@@ -21,3 +21,14 @@ rl.question("Enter your choice (1-3): ", choice => {
     
         rl.question("Please enter a number for factorial : ", numStr => {
             let num = parseInt(numStr);
+
+            rl.question("Enter your choice (1-3): ", choice => {
+    console.log(choice);
+
+    if (choice === '1') {
+        console.log("Hello, User!");
+    } else if (choice === '2') {
+        console.log("Factorial");
+    
+        rl.question("Please enter a number for factorial : ", numStr => {
+            let num = parseInt(numStr);
