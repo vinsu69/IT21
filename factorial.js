023d21fe53c47718f16eb0aa0f11161a994c0316
vinsu69 <joewinmante@gmail.com>
2025-09-05 20:09:31 +0800

    initial

---
+++ factorial.js
@@ -0,0 +1,12 @@
+const readline = require('readline');
+
+const rl = readline.createInterface({
+    input: process.stdin,
+    output: process.stdout
+});
+console.log("=================================");
+console.log("Factorial Application");
+console.log("1. Say Hello");
+console.log("2. Factorial");
+console.log("3. Exit Application");
+console.log("=================================");
