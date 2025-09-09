b57d40b371d7a833dcb1995441de798c0eecdd44
vinsu69 <mantejoewin@gmail.com>
2025-09-09 21:59:58 +0800

    Added backToMenu() function and initial instance

--- factorial.js
+++ factorial.js
@@ -4,6 +4,8 @@ const rl = readline.createInterface({
     input: process.stdin,
     output: process.stdout
 });
+function mainMenu(){
+
 console.log("=================================");
 console.log("Factorial Application");
 console.log("1. Say Hello");
@@ -11,17 +13,20 @@ console.log("2. Factorial");
 console.log("3. Exit Application");
 console.log("=================================");
 
-rl.question("Enter your choice (1-3): ", choice => {
-    console.log(choice);
+ rl.question("Enter your choice (1-3): ", choice =>{   
 
     if (choice === '1') {
-        console.log("Hello, User!");
+        say Hello();
     } else if (choice === '2') {
-        console.log("Factorial");
-    
-        rl.question("Please enter a number for factorial : ", numStr => {
-            let num = parseInt(numStr);
+        computeFactorial();
+    }else if (choice === '3') {
+        exit Program();
+    }else {
+        console.log("Invalid Choice.Please try again");
+    }
 
+})
+}
             rl.question("Enter your choice (1-3): ", choice => {
     console.log(choice);
 
